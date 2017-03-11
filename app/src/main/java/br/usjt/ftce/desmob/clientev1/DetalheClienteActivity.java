59bd7d55d4d61a9ed76ee4004dd1f3b93a7db180
arqdsis <arqdsis>
2017-03-10 22:39:53 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/ftce/desmob/clientev1/DetalheClienteActivity.java
@@ -0,0 +1,21 @@
+package br.usjt.ftce.desmob.clientev1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetalheClienteActivity extends AppCompatActivity {
+    TextView textViewNome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe_cliente);
+        textViewNome = (TextView) findViewById(R.id.txt_cliente_nome);
+        Intent intent = getIntent();
+        String nome = intent.getStringExtra(ListarClienteActivity.NOME);
+        textViewNome.setText(nome);
+
+
+    }
+}
